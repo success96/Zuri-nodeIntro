3e66d810f55c74764edd3d689032bbeca62531cb
Success96 <lfawas6@gmail.com>
2022-10-29 15:41:19 +0100

    Switched from using express to http module

--- Intro-node.js
+++ Intro-node.js
@@ -1,24 +1,73 @@
-const express = require("express");
+const fs = require('fs');
+const path = require('path');
+const http = require('http');
 
-const app = express();
+
+
+const server = http.createServer((request, response) =>{
+
+  //get:localhost:3000/
+  if (request === '/'){
+    let filePath = path.join(__dirname, 'home.html');
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) console.log(err.message);
+      response.writeHead(200, {'content-type': 'text/html'});
+      response.end(data);
+    });
+  };
+
+  //get:locahost:3000/home
+  if (request === '/home'){
+    let filePath = path.join(__dirname, 'home.html');
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) console.log(err.message);
+      response.writeHead(200, {'content-type': 'text/html'});
+      response.end(data);
+    });
+  };
+
+  //get:localhost:3000/about
+  if (request === '/about'){
+    let filePath = path.join(__dirname, 'about.html');
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) console.log(err.message);
+      response.writeHead(200, {'content-type': 'text/html'});
+      response.end(data);
+    });
+  };
+
+  //get:localhost:3000/contact
+  if (request === '/contact'){
+    let filePath = path.join(__dirname, 'contact.html');
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) console.log(err.message);
+      response.writeHead(200, {'content-type': 'text/html'});
+      response.end(data);
+    });
+  };
+
+
+})
+
+//const app = express();
 const port = 3000;
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-app.get("/", (req, res)=>{
-  res.sendFile(__dirname+ "/home.html")
-});
+// app.get("/", (req, res)=>{
+//   res.sendFile(__dirname+ "/home.html")
+// });
 
-app.get("/home", (req, res)=>{
-  res.sendFile(__dirname+ "/home.html")
-});
+// app.get("/home", (req, res)=>{
+//   res.sendFile(__dirname+ "/home.html")
+// });
 
-app.get("/about", (req, res)=>{
-  res.sendFile(__dirname+ "/about.html")
-});
+// app.get("/about", (req, res)=>{
+//   res.sendFile(__dirname+ "/about.html")
+// });
 
-app.get("/contact", (req, res)=>{
-  res.sendFile(__dirname+ "/contact.html")
-});
+// app.get("/contact", (req, res)=>{
+//   res.sendFile(__dirname+ "/contact.html")
+// });
